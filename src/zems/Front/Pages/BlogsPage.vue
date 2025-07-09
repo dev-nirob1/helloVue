1228d2a0f0d8db671757309c677ff515af61b28f
dev-nirob1 <dev.nirob1@gmail.com>
2025-07-09 23:07:30 +0600

    blogs page data fetching

--- src/zems/Front/Pages/BlogsPage.vue
+++ src/zems/Front/Pages/BlogsPage.vue
@@ -1,3 +1,29 @@
+<script setup>
+import BaseTitle from '@/components/Elements/BaseTitle.vue';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import BlogCard from '../Components/Widget/BlogCard.vue';
+const blogs = ref([])
+
+const fetchBlogs = async () => {
+    const res = await axios.get('https://dev.to/api/articles?per_page=3&page=1')
+    blogs.value = res.data
+}
+onMounted(fetchBlogs)
+</script>
+
 <template>
-    blogs page
+    <div class="blogs">
+        <div class="container">
+            <BaseTitle class="text-center mb-3">All Blogs</BaseTitle>
+            <div class="medium-2 large-3 gap-2">
+                <BlogCard v-for="blog in blogs" :key="blog.id" :data="blog"></BlogCard>
+            </div>
+        </div>
+    </div>
 </template>
+<style scoped>
+.blogs {
+    padding: 3.75rem 0;
+}
+</style>
